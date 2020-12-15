b9f06c58c691981779bc599becce4001ad9d0dca
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-12-15 14:26:49 -0600

    oved router and seitch scope

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <Switch>
+        <App />
+      </Switch>
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
